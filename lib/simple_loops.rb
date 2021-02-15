d81f9b24608d9da550713576880598cc25ed019c
Colten Appleby <colten96@gmail.com>
2021-02-15 01:41:31 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,39 @@
 # Write your methods here
+
+def loop_message_five_times(string)
+  counter = 0
+  while counter < 5 do
+    puts string
+    counter += 1
+  end
+end
+
+
+def loop_message_n_times(string, n)
+  counter = 0
+  while counter < n do
+    puts string
+    counter += 1 
+  end
+end
+
+
+def output_array(array)
+  i = 0
+  while array[i]  do
+    puts array[i]
+    i += 1 
+  end
+  
+end
+
+
+def return_string_array(array)
+  i = 0 
+  str = ''
+  while array[i] do
+    str += array[i]
+    i += 1 
+  end
+  
+end 
